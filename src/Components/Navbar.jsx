f32fce97612a63102c0efdc4972aabcb20cce34c
jaypee-0 <eyibrajohnpaul@gmail.com>
2022-05-08 18:20:14 +0100

    First Commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { FontAwesomeIcon as FA } from '@fortawesome/react-fontawesome'
 
 const Navbar = () => {
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+
   return (
-    <nav className='pt-1 expand'>
-        <h2 className='fw-bold pb-5'><span className='text-danger'>Dev</span>challenges.io</h2>
+    <nav className='pt-1 expand'>     
+        <div className='d-flex'>
+        <h2 className='fw-bold pb-5'><span className='text-danger'>Dev</span>challenges.io</h2> 
+        <span onClick={handleClick}><FA className='pt-4' icon={click ? 'times': 'bars'} /></span>  
+        </div>
         <ul className='pl-0'>
             <Link to='/buttons'><li className='nav-items'>Colors</li></Link>
             <Link to='/buttons'><li className='nav-items'>Typography</li></Link>
