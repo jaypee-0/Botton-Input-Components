cbd4bfb9a6bc793420343f5b73293d16c9b49837
jaypee-0 <eyibrajohnpaul@gmail.com>
2022-05-08 18:02:04 +0100

    First Commit

--- src/Components/Btn.jsx
+++ src/Components/Btn.jsx
@@ -6,6 +6,9 @@ const sizes = ['sm', 'md', 'lg']
 const colors = ['default-color', 'outline', 'text', 'primary', 'secondary', 'danger']
 const disable = ['disabled', 'enabled']
 const shadow = ['disableshadow', 'disableshadowNull']
+const StartIcon = ['null', 'start-shopping-cart']
+const EndIcon = ['null', 'end-shopping-cart']
+
 
 const Buttons = ({variant, size, color, disabled, disabledShadow, startIcon, endIcon}) => {
         const Variant = Variants.includes(variant) ? variant : Variants[0];
@@ -13,8 +16,10 @@ const Buttons = ({variant, size, color, disabled, disabledShadow, startIcon, end
         const Color = colors.includes(color) ? color : colors[0];  
         const Disabled = disable.includes(disabled) ? disabled : disable[1];  
         const Shadow = shadow.includes(disabledShadow) ? disabledShadow : shadow[1];
+        const StartIcons = StartIcon.includes(startIcon) ? startIcon : StartIcon[0];
+        const EndIcons = EndIcon.includes(endIcon) ? endIcon : EndIcon[0];
   return (
-    <button className={`btnn ${Variant} ${Size} ${Color} ${Disabled} ${Shadow}`}>Default</button>
+    <button className={`btnn ${Variant} ${Size} ${Color} ${Disabled} ${Shadow} ${StartIcons} ${EndIcons}`}>Default</button>
   )
 }
 
--- src/Components/Buttons.jsx
+++ src/Components/Buttons.jsx
@@ -15,24 +15,24 @@ const Buttons = () => {
             <div className="col-12"><pre className="htmlcode">&lt;Btn variant="text"/ &gt;</pre><Btn variant="text" color="text"/></div>      
         </div>
         <div className="d-flex flex-column flex-md-row mb-5">
-            <div className="col-md-3"><pre className="htmlcode">&lt;Btn disabledShadow=""/ &gt;</pre><Btn disabledShadow="disableshadow"/></div>    
-            <div className="col-md-3"><pre className="htmlcode">&lt;Btn disabled="disabled"/ &gt;</pre><Btn disabled="disabled" variant="text"/></div>  
+            <div className="col-12 col-md-3"><pre className="htmlcode">&lt;Btn disabledShadow=""/ &gt;</pre><Btn disabledShadow="disableshadow"/></div>    
+            <div className="col-12 col-md-3"><pre className="htmlcode">&lt;Btn disabled="disabled"/ &gt;</pre><Btn disabled="disabled" variant="text"/></div>  
             <div className="col-12 col-md-4"><pre className="htmlcode">&lt;Btn variant="text" disabled="disabled/ &gt;</pre><Btn variant="text" color="text" disabled="disabled" /></div>
         </div>
-        <div className="d-flex mb-5">
-            <div className="col-4"><pre className="htmlcode">&lt;Btn variant="text"/ &gt;</pre><Btn color="primary"/></div>    
-            <div className="col-4"><pre className="htmlcode">&lt;Btn variant="text"/ &gt;</pre><Btn color="primary"/></div>    
+        <div className="d-flex flex-column flex-md-row mb-5">
+            <div className="col-md-6 col-lg-4"><pre className="htmlcode">&lt;Btn startIcon="start-shopping-cart"/ &gt;</pre><Btn startIcon='start-shopping-cart' /></div>    
+            <div className="col-md-6 col-lg-4"><pre className="htmlcode">&lt;Btn endIcon="end-shopping-cart"/ &gt;</pre><Btn endIcon='end-shopping-cart'/></div>    
         </div>
-        <div className="d-flex mb-5">
-            <div className="col-3"><pre className="htmlcode">&lt;Btn size="sm"/ &gt;</pre><Btn color="primary" size="sm"/></div>    
-            <div className="col-3"><pre className="htmlcode">&lt;Btn size="md"/ &gt;</pre><Btn color="primary" size="md"/></div>  
-            <div className="col-3"><pre className="htmlcode">&lt;Btn size="lg"/ &gt;</pre><Btn color="primary" size="lg"/></div>
+        <div className="d-flex flex-column flex-md-row mb-5">
+            <div className="col-12 col-md-3 "><pre className="htmlcode">&lt;Btn size="sm"/ &gt;</pre><Btn color="primary" size="sm"/></div>    
+            <div className="col-12 col-md-3 "><pre className="htmlcode">&lt;Btn size="md"/ &gt;</pre><Btn color="primary" size="md"/></div>  
+            <div className="col-12 col-md-3 "><pre className="htmlcode">&lt;Btn size="lg"/ &gt;</pre><Btn color="primary" size="lg"/></div>
         </div>
-        <div className="d-flex mb-5">
-            <div className="col-3"><pre className="htmlcode">&lt;Btn color="default-color"/ &gt;</pre><Btn color="default-color"/></div>    
-            <div className="col-3"><pre className="htmlcode">&lt;Btn color="primary"/ &gt;</pre><Btn color="primary"/></div>  
-            <div className="col-3"><pre className="htmlcode">&lt;Btn color="secondary"/ &gt;</pre><Btn color="secondary"/></div> 
-            <div className="col-3"><pre className="htmlcode">&lt;Btn color="danger"/ &gt;</pre><Btn color="danger"/></div>     
+        <div className="d-flex flex-column flex-md-row mb-5">
+            <div className="col-12 col-md-3"><pre className="htmlcode">&lt;Btn color="default-color"/ &gt;</pre><Btn color="default-color"/></div>    
+            <div className="col-12 col-md-3"><pre className="htmlcode">&lt;Btn color="primary"/ &gt;</pre><Btn color="primary"/></div>  
+            <div className="col-12 col-md-3"><pre className="htmlcode">&lt;Btn color="secondary"/ &gt;</pre><Btn color="secondary"/></div> 
+            <div className="col-12 col-md-3"><pre className="htmlcode">&lt;Btn color="danger"/ &gt;</pre><Btn color="danger"/></div>     
         </div>  
     </div>
   )
